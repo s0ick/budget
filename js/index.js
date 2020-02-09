03fdaf81e9512111d7a489a3abab0ea7bc1c758d
s0ick <36438226+s0ick@users.noreply.github.com>
2020-02-09 12:41:43 +0300

    fix: reset | js

--- js/index.js
+++ js/index.js
@@ -74,6 +74,21 @@ let start = document.getElementById('start'),
           item.value = '';
         }
       });
+
+      this.income = {};
+      this.addIncome = [];
+      this.expenses = {};
+      this.addExpenses = [];
+      this.deposit = false;
+      this.percentDeposit = 0;
+      this.moneyDeposit = 0;
+      this.budget = 0;
+      this.budgetDay = 0;
+      this.budgetMonth = 0;
+      this.expensesMonth = 0;
+      this.incomeMonth = 0;
+
+      start.disabled = true;
     },
 
     showResult: function() {
